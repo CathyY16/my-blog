9ca0479a60235f587c5240e46fb5730fc07133df
CathyY16 <hong208@hotmail.com>
2019-11-19 23:42:08 -0500

    First Commit

---
+++ src/build/precache-manifest.6befcd69d35ff9394bce8a6fdd7a02a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ceb569b2d1fbd34240736fad4d9e638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "949f5c71301708106aab",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "5b2a25802558aef4f78c",
+    "url": "/static/js/2.91cff5f8.chunk.js"
+  },
+  {
+    "revision": "949f5c71301708106aab",
+    "url": "/static/js/main.36bd2ba9.chunk.js"
+  },
+  {
+    "revision": "35a7acf4fb928453b799",
+    "url": "/static/js/runtime-main.8727d782.js"
+  }
+]);
